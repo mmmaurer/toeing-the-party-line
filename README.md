a21feab01cd042fa9bda43f8f1938d0f96e2edde
Maximilian Maurer <maximilian.maurer@gesis.org>
2024-10-07 17:04:24 +0200

    modify README

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-# toeing-the-party-line
+# Toeing the Party Line: Election Manifestos as a Key to Understand Political Discourse on Twitter
+
+This repository contains the code and data for the paper "Toeing the Party Line: Election Manifestos as a Key to Understand Political Discourse on Twitter".
+
+To run any code from this repository, install the packages from `requirements.txt`.
+
+Note that to run any of our code, we assume certain naming conventions (e.g. datasets to be named `btw{year}_tweets.csv`) and columns to be present and named accordingly. In particular, the column with the Tweet text should be called `text`. For any other meta-data necessary for replication, we provide it in the respective experiment data in this repository. 
+
+Since we are using Polars for efficient data handling and processing, you may want to adapt the `DTYPES` dictionary in [`src/data_processing.py`](src/data_processing.py) to fit your data.
+## Model availability
+
+The models are available through the Hugging Face Hub under 
+- [`mmmaurer/sbert-hashtag-german-politicians-2017`](https://huggingface.co/mmmaurer/sbert-hashtag-german-politicians-2017)
+- [`mmmaurer/sbert-hashtag-german-politicians-2021`](https://huggingface.co/mmmaurer/sbert-hashtag-german-politicians-2017)
+
+## Data availability
+We provide Tweet IDs and additional meta-information necessary to replicate our experiments in [`data/`](data/).
+To retrieve the individual collections, see [https://doi.org/10.4232/1.12319](https://doi.org/10.4232/1.12319) for 2013, [https://doi.org/10.4232/1.12992](https://doi.org/10.4232/1.12992) for 2017, and [https://doi.org/10.4232/1.14233](https://doi.org/10.4232/1.14233) for 2021.
+
+# Hashtag Fine-Tuning Sentence Transformers
+To finetune models as described in the paper, create training and validation datasets such that the given label for a positive example sentence pair (i.e. two sentences that have at least one common hashtag) is $1$ and of a negative example sentence pair (i.e. two sentences that do not have a co-occurring hashtag) has the label $0$.
+
+We provide a script for gathering such datasets:
+
+```
+python3 gather_dataset.py \
+        --data_path [path to your dataset dir] \
+        --year_lower_bound [first year to consider for sampling Tweets] \
+        --year_upper_bound [last year to consider for sampling Tweets] \
+        --n_partes [Minimum number of parties a hashtag should occur for] \
+        --n_tweets [Minimum number of Tweets a hashtag should occur in] \
+        --year [Year of the dataset] \
+        --additional_filename [Optional additional marker to append to the
+                               dataset name]
+```
+The data (Tweet IDs and label per sentence pair) for replicating our models is available in [`data/finetuning/`](data/finetuning/).
+
+To train a model with the resulting datasets, run
+
+```
+python3 finetune_sbert.py \
+        --data_path [path to your datasets dir] \
+        --year [dataset target year] \
+        --output_path [path to output dir for the trained model] \
+        --batch_size [batch size] \
+        --epochs [epochs] \
+        --warmup_steps [warmup steps] \
+        --pool [ids of the GPUs in pool, separated with "-", e.g. 1-2-3] \
+        --cuda [if you use a single GPU, indicate the id, e.g. 0]
+```
+
+# Replicating our experiments
+
+Per experiment, the data (Tweet IDs and necessary meta-data) is available in [`data/`](data/) under a subdirectory with the respective experiment number (e.g. [`data/experiment1/`](data/experiment1/))
+
+To replicate our experiments, first retrieve embeddings for your given model using 
+```
+python3 embeddings.py --data_dir [path to your dataset dir] \
+                      --output_dir [output path for the pickled embeddings] \
+                      --model [model name to gather embeddings for] \
+                      --year [dataset target year] \
+                      --cache_dir [path to your transformers cache dir]
+```
+
+For our robustness experiments, the resulting embeddings can be filtered using
+
+```
+python3 filter_embeddings --model [model name] \
+                          --embeddings_path [path to the pickled embeddings] \
+                          --data_path [path to the dataset dir] \
+                          --year [dataset target year] \
+                          --mode [filtering mode, see below] \
+                          --month_start [start month, only used in time mode] \
+                          --month_end [end month, only used in time mode] \
+                          --parties [list of party abbreviations] \
+                          --seed [random seed, only used in random mode] \
+                          --fraction [fraction of data to keep, used in random mode]
+```
+
+For experiment 2, use mode `random` with the seeds $0$, $6$, $12$, $24$, $42$ with fractions from $0.125$ to $1.0$ in $0.125$ stepsize steps. For experiment 3 (a) mode `time` with `start_month` $1$, $3$, $6$ and `end_month` $9$, and individual months (i.e. `start_month` $1$, `end_month` $1$). For experiment 4, use `mode`s $elected$, $incumbent$, $reelected$, $newly\_elected$, and $not\_reelected$.
+
+To run the evaluation in the experiments, the manifesto dataset is needed. To download the version we used in the paper, visit [https://doi.org/10.25522/manifesto.mpds.2023a](https://doi.org/10.25522/manifesto.mpds.2023a). For the latest version, visit [https://manifesto-project.wzb.eu/datasets](https://manifesto-project.wzb.eu/datasets).
+
+Finally, to run the experiments, use
+
+```
+python3 experiments.py --embeddingspath [path to the pickled embeddings] \
+                       --manifestopath [path to the manifesto csv] \
+                       --datapath [path to the dataset dir] \
+                       --outputpath [path to output the results to] \
+                       --year [Election year to run experiment for] \
+                       --mode [experiment mode] \
+                       --seed [random seed for random mode] \
+                       --fraction [fraction of tweets to choose randomly] \
+                       --start_month [start month for time mode] \
+                       --end_month [end month for time mode]
+                       --parties [list of party abbreviations]
+```
+The `mode` and additional mode-specific settings should be according to how the embeddings were filtered.
+
+# Citation
+A bibtex reference to copy will be added upon publication.
---
+++ filter_embeddings.py
@@ -0,0 +1,191 @@
+import argparse
+import os
+import pickle
+import numpy.random as random
+
+import polars as pl
+from tqdm import tqdm
+
+# import data_processing
+from src import data_processing
+
+def get_month_filter_columns(df, month_start, month_end):
+    df = data_processing.get_month(df)
+    df = df.with_columns(
+        ((pl.col("month") <= month_end) & \
+         (pl.col("month") >= month_start)).alias("filter_column")
+    )
+    return df
+
+def get_random_filter_columns(df, fraction, seed):
+    random.seed(seed)
+    df = df.with_columns(
+        pl.lit(random.choice([True, False], len(df), p=[fraction, 1-fraction])).alias("filter_column")
+    )
+    return df
+
+def get_elected_filter_columns(df):
+    df = df.with_columns(
+        (pl.col("elected") == 1).alias("filter_column")
+    )
+    return df
+
+def get_reelected_filter_columns(df):
+    df = df.with_columns(
+        ((pl.col("elected") == 1) & \
+         (pl.col("incumbent") == 1)
+         ).alias("filter_column"),
+    )
+    return df
+
+def get_newly_elected_filter_columns(df):
+    df = df.with_columns(
+        ((pl.col("elected") == 1) & \
+         (pl.col("incumbent") == 0)
+         ).alias("filter_column"),
+    )
+    return df
+
+def get_not_reelected_filter_columns(df):
+    df = df.with_columns(
+        ((pl.col("elected") == 0) & \
+         (pl.col("incumbent") == 1)
+         ).alias("filter_column"),
+    )
+    return df
+
+def get_content_filter_columns(df):
+    df = df.with_columns(
+        ((pl.col("is_retweet") == 0) & \
+         (pl.col("is_quoted") == 0) & \
+         (pl.col("is_reply") == 0) & \
+         (pl.col("text").str.ends_with("…") == 0)
+        ).alias("filter_column")
+    )
+    return df
+
+def get_incumbent_filter_columns(df):
+    df = df.with_columns(
+        (pl.col("incumbent") == 1).alias("filter_column")
+    )
+    return df
+
+def filter_embeddings(df, embeddings, parties):
+    embeddings_filtered = []
+    for i, party in tqdm(enumerate(parties), total=len(parties)):
+        party_df = df.filter(pl.col("faction") == party)
+        assert len(party_df) == len(embeddings[i]), \
+            "Mismatch between embeddings and data, check order of parties"
+        party_embeddings = [embeddings[i][j] for j in range(len(party_df)) if \
+                            party_df["filter_column"].to_list()[j]]
+        embeddings_filtered.append(party_embeddings)
+    return embeddings_filtered
+
+def filter_embeddings_random(df, embeddings, parties, p_true=0.5):
+    embeddings_filtered = []
+    for i, party in tqdm(enumerate(parties), total=len(parties)):
+        party_df = df.filter(pl.col("faction") == party)
+        filter_list = [random.choice([True, False], p=[p_true, 1-p_true]) for \
+                       _ in range(len(party_df))]
+        assert len(party_df) == len(embeddings[i]), \
+            "Mismatch between embeddings and data, check order of parties"
+        party_embeddings = [embeddings[i][j] for j in range(len(party_df)) if \
+                            filter_list[j]]
+        embeddings_filtered.append(party_embeddings)
+    return embeddings_filtered
+
+def save_filtered_embeddings(embeddings, model, year, month_start, month_end):
+    if not os.path.exists("./embeddings/filtered"):
+        os.makedirs("./embeddings/filtered")
+    with open(f"./embeddings/filtered/{model}-{year}-{month_start}-"
+              f"{month_end}.pkl", "wb") as f:
+        pickle.dump(embeddings, f)
+
+def save_filtered_embeddings_random(embeddings, model, year, fraction, seed):
+    if not os.path.exists("./embeddings/random-choice"):
+        os.makedirs("./embeddings/random-choice")
+    with open(f"./embeddings/random-choice/{model}-{year}-random-{fraction}-{seed}.pkl", "wb") as f:
+        pickle.dump(embeddings, f)
+
+def save_filtered_embeddings_content(embeddings, model, year):
+    if not os.path.exists("./embeddings/content"):
+        os.makedirs("./embeddings/content")
+    with open(f"./embeddings/content/{model}-{year}-content.pkl", "wb") as f:
+        pickle.dump(embeddings, f)
+
+def save_filtered_embeddings_elected(embeddings, model, year):
+    if not os.path.exists("./embeddings/elected"):
+        os.makedirs("./embeddings/elected")
+    with open(f"./embeddings/elected/{model}-"
+              f"{year}.pkl", "wb") as f:
+        pickle.dump(embeddings, f)
+
+def save_filtered_embeddings_incumbent(embeddings, model, year):
+    if not os.path.exists("./embeddings/incumbent"):
+        os.makedirs("./embeddings/incumbent")
+    with open(f"./embeddings/incumbent/{model}-{year}.pkl", "wb") as f:
+        pickle.dump(embeddings, f)
+
+def save_filtered_embeddings(embeddings, model, year, mode):
+    if not os.path.exists(f"./embeddings/{mode}"):
+        os.makedirs(f"./embeddings/{mode}")
+    with open(f"./embeddings/{mode}/{model}-{year}.pkl", "wb") as f:
+        pickle.dump(embeddings, f)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--embeddings_path", type=str, required=True)
+    parser.add_argument("--data_path", type=str, required=True)
+    parser.add_argument("--year", type=int, required=True)
+    parser.add_argument("--mode", type=str, required=True,
+                        help="Filter embeddings by time or random choice")
+    parser.add_argument("--fraction", type=float, default=0.5,
+                        help="Fraction of data to keep; only used in random mode")
+    parser.add_argument("--month_start", type=int, default=2,
+                        help="Month to start filtering from; only used in time mode")
+    parser.add_argument("--month_end", type=int, default=8,
+                        help="Month to end filtering; only used in time mode")
+    parser.add_argument("--parties", nargs="+",
+                        default=['AfD', 'CDU/CSU', 'FDP', 'GRÜNE', 'DIE LINKE', 'SPD'])
+    parser.add_argument("--seed", type=int, default=42)
+    args = parser.parse_args()
+
+    df = data_processing.load_data(args.data_path, args.year)
+    embeddings = pickle.load(open(args.embeddings_path, "rb"))
+
+    if args.mode == "time":
+        df = get_month_filter_columns(df, args.month_start, args.month_end)
+        embeddings_filtered = filter_embeddings(df, embeddings, args.parties)
+        save_filtered_embeddings(embeddings_filtered, args.model, args.year,
+                                 args.month_start, args.month_end)
+    elif args.mode == "random":
+        df = get_random_filter_columns(df, args.fraction, args.seed)
+        embeddings_filtered = filter_embeddings(df, embeddings, args.parties)
+        save_filtered_embeddings_random(embeddings_filtered, args.model,
+                                        args.year, args.fraction, args.seed)
+    elif args.mode == "content":
+        df = get_content_filter_columns(df)
+        embeddings_filtered = filter_embeddings(df, embeddings, args.parties)
+        save_filtered_embeddings_content(embeddings_filtered, args.model, args.year)
+    elif args.mode == "elected":
+        df = get_elected_filter_columns(df)
+        embeddings_filtered = filter_embeddings(df, embeddings, args.parties)
+        save_filtered_embeddings_elected(embeddings_filtered, args.model, args.year)
+    elif args.mode == "incumbent":
+        df = get_incumbent_filter_columns(df)
+        embeddings_filtered = filter_embeddings(df, embeddings, args.parties)
+        save_filtered_embeddings_incumbent(embeddings_filtered, args.model, args.year)
+    elif args.mode == "reelected":
+        df = get_reelected_filter_columns(df)
+        embeddings_filtered = filter_embeddings(df, embeddings, args.parties)
+        save_filtered_embeddings(embeddings_filtered, args.model, args.year, "reelected")
+    elif args.mode == "newly_elected":
+        df = get_newly_elected_filter_columns(df)
+        embeddings_filtered = filter_embeddings(df, embeddings, args.parties)
+        save_filtered_embeddings(embeddings_filtered, args.model, args.year, "newly-elected")
+    elif args.mode == "not_reelected":
+        df = get_not_reelected_filter_columns(df)
+        embeddings_filtered = filter_embeddings(df, embeddings, args.parties)
+        save_filtered_embeddings(embeddings_filtered, args.model, args.year, "not-reelected")
+
